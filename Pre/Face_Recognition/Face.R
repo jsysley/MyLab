24ab58db5d112d1c4aaf7741899611e902560258
jsysley <237149516@qq.com>
2017-03-28 22:50:20 +0800

    2017.03.28,Windows

--- Pre/Face_Recognition/Face.R
+++ Pre/Face_Recognition/Face.R
@@ -1,4 +1,4 @@
-dir_path <- "/Users/jsysley/Desktop/多元统计/Pre/Face_Recognition"
+dir_path <- "F:/git/trial/R/Pre/Face_Recognition"
 if(!require(bmp))install.packages("bmp")
 require(bmp)
 read_bmp <- function(dir_path,i)
@@ -7,12 +7,33 @@ read_bmp <- function(dir_path,i)
     res <- read.bmp(file_name)
     return(res[,,1])
 }
-face_data <- sapply(1:11, read_bmp,dir_path=dir_path)
+face_data <- t(sapply(1:11, read_bmp,dir_path=dir_path))
 dim(face_data)#每列为一张
-#每列减去均值
+
+if(!require(pixmap))install.packages("pixmap")
+require(pixmap)
+
+#Plot the “average” face
 m <- colMeans(face_data)
-if(!require(pixmapRGB))install.packages("pixmapRGB")
-require(pixmapRGB)
 pr=pixmapRGB(matrix(m,nrow=243),ncol=320)
 plot(pr)
+#Center the data by columns
 face_data_ss <- t(apply(face_data,1,function(x)return(x-m)))
+#Calculate the correlation matrix
+A <- face_data_ss%*%t(face_data_ss)
+#Calculate the eigenvalues and eigenvextors
+eigen_value <- eigen(A)
+
+#Plot the pca components plot
+par(mfrow=c(3,4))
+xnew <- m
+pr=pixmapRGB(matrix(xnew,nrow=243),ncol=320)
+plot(pr)
+for(i in 1:11)
+{
+    v <- t(face_data_ss)%*%eigen_value$vector[,i]/sqrt(abs(eigen_value$values[i]))
+    b <- t(v)%*%face_data_ss[2,]
+    xnew <- xnew+v%*%b
+    pr <- pixmapRGB(matrix(xnew,nrow=243,ncol=320))
+    plot(pr)
+}
--- Pre/The_Nutritional_of_food/Food.R
+++ Pre/The_Nutritional_of_food/Food.R
@@ -1,4 +1,4 @@
-dir_path <- "/Users/jsysley/Desktop/多元统计/Pre/The_Nutritional_of_food"
+dir_path <- "F:/git/trial/R/Pre/The_Nutritional_of_food"
 raw_data <- read.table(paste(dir_path,"/food.data.txt",sep=""),header = TRUE,stringsAsFactors = FALSE)
 #######################################################
 #######################################################
@@ -13,12 +13,13 @@ if(!require(reshape))install.packages("reshape")
 require(ggplot2)
 require(reshape)
 #数据格式转换
-Plot_Density <- function(use_data)
+Plot_Density <- function(use_data,xtart=0,xend=10,ystart=0,yend=1)
 {
     id <- 1:nrow(use_data)
     long_data <- cbind(use_data,id)
     long_data <- melt(long_data,id="id")
-    return(long_data)
+    ggplot(long_data,aes(x=value,colour=variable))+geom_density()+
+        xlim(0,xend)+ylim(0,yend)
 }
 
 Plot_A_Density <- function(name,use_data)
@@ -27,17 +28,9 @@ Plot_A_Density <- function(name,use_data)
     plot(density(use),xlab=name,main="density line")
 }
 #查看所有数据密度曲线
-long_data <- Plot_Density(use_data)
-#所有密度曲线
-ggplot(long_data,aes(x=value,colour=variable))+geom_density()+
-    xlim(0,xend)+ylim(0,yend)
-#单独密度曲线
+Plot_Density(use_data,xend=1000)
 lapply(colnames(use_data),Plot_A_Density,use_data=use_data)
-#箱线图
-ggplot(long_data,aes(x=variable,y=value,colour=variable))+geom_boxplot(varwidth = TRUE)
-#删除一个变量（量纲问题）
-ggplot(long_data[long_data[,2]!='Food.energy.calories',],aes(x=variable,y=value,colour=variable))+geom_boxplot(varwidth = TRUE)
-ggplot(long_data[!long_data[,2]%in%c('Food.energy.calories','weight.grams'),],aes(x=variable,y=value,colour=variable))+geom_boxplot(varwidth = TRUE)
+
 #查看各个变量分布值情况
 if(!require(Hmisc))install.packages("Hmisc")
 require(Hmisc)
@@ -48,8 +41,9 @@ require(mice)
 md.pattern(use_data)
 #######################################################
 #######################################################
+div <- function(x,y) return(x/y)
 #除以wight.grams
-use_data_div <- apply(use_data,2,function(x,y)return(x/use_data[,"weight.grams"]))
+use_data_div <- apply(use_data,2,div,y=use_data[,"weight.grams"])
 #移除wight.grams变量
 use_data_div <- use_data_div[,-which("weight.grams"==colnames(use_data_div))]
 #标准化
@@ -58,5 +52,5 @@ use_data_div_ss <- scale(use_data_div)
 result <- princomp(use_data_div_ss)
 summary(result)
 result$loadings
-result$scores#得分矩阵
+result$scores
 plot(result,type="l")
